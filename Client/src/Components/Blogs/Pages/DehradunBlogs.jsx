2510e9e1d739b7c56f585f825055e6d36b8dd576
Ketan9548 <kc918062@gmail.com>
2025-01-15 17:26:07 +0530

    Done

--- Client/src/Components/Blogs/Pages/DehradunBlogs.jsx
+++ Client/src/Components/Blogs/Pages/DehradunBlogs.jsx
@@ -1,56 +1,166 @@
-import React from "react";
-import Deharadunimg from "../Blogsimges/Dehardun.jpg";
+import React, { useEffect, useState } from "react";
+import img1 from "../Carosalimg/images1.jpg";
+import img2 from "../Carosalimg/images2.jpg";
+import img3 from "../Carosalimg/images3.jpg";
+import img4 from "../Carosalimg/images4.jpg";
+import img5 from "../Carosalimg/images5.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEarth } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faPersonHiking } from "@fortawesome/free-solid-svg-icons";
+import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faSignal } from "@fortawesome/free-solid-svg-icons";
 
 const DehradunBlogs = () => {
+  const [duration, setduration] = useState(0);
+  const [altitude, setaltitude] = useState(0);
+  const [trakingarea, settrakingarea] = useState(0);
+
+  const controler = (setcontroler, target, increment, controlertime) => {
+    const interval = setInterval(() => {
+      setcontroler((prev) => {
+        if (prev >= target) {
+          clearInterval(interval);
+          return target;
+        }
+        return prev + increment;
+      });
+    }, controlertime);
+
+    return () => clearInterval(interval);
+  };
+
+  useEffect(() => {
+    controler(setduration, 3, 0.5, 60);
+  }, []);
+  useEffect(() => {
+    controler(setaltitude, 1427, 10, 1);
+  }, []);
+  useEffect(() => {
+    controler(settrakingarea, 12, 1, 50);
+  }, []);
+
+  const carosal = [img1, img2, img3, img4, img5];
   return (
     <>
-      <div className="h-screen w-screen flex justify-center items-center">
-        <div className="w-[74%] h-[80%]">
-          <img
-            className="w-full h-full object-cover"
-            src={Deharadunimg}
-            alt="Nanital"
-          />
+      <div>
+        <div
+          id="default-carousel"
+          className="relative w-full"
+          data-carousel="slide"
+        >
+          <div className="relative h-56 overflow-hidden md:h-96">
+            {carosal.map((imge, index) => (
+              <div
+                key={index}
+                className="hidden duration-700 ease-in-out"
+                data-carousel-item
+              >
+                <img
+                  src={imge}
+                  className="w-full h-full object-cover shadow-lg  "
+                  alt={`Slide ${index + 1}`}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+            {[...Array(carosal.length)].map((_, idx) => (
+              <button
+                key={idx}
+                type="button"
+                className="w-3 h-3 rounded-full"
+                aria-current={idx === 0 ? "true" : "false"}
+                aria-label={`Slide ${idx + 1}`}
+                data-carousel-slide-to={idx}
+              ></button>
+            ))}
+          </div>
+          <button
+            type="button"
+            className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-prev
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+          <button
+            type="button"
+            className="absolute top-0  end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+            data-carousel-next
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+              <svg
+                className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row w-full px-6 md:px-24 mt-8 gap-8">
-        <div className="flex-1 w-1/2">
-          <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-6">
-            City Near Nanital
-          </h1>
-          <p className="text-lg md:text-xl font-serif leading-relaxed text-gray-600 mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
-            dicta cupiditate ex debitis ipsum labore nam animi magnam qui vero
-            nihil officiis eaque omnis vitae fugiat, voluptates nostrum eius
-            exercitationem quod repellendus?
+      <div className="flex flex-row justify-around">
+        <div className="mt-3 mb-3 flex flex-col justify-center items-center">
+          <p className=" text-red-500 opacity-85 text-4xl">
+            <FontAwesomeIcon icon={faEarth} />
           </p>
-          {Array(9)
-            .fill(null)
-            .map((_, index) => (
-              <React.Fragment key={index}>
-                <h2 className="text-2xl font-semibold text-gray-700 mt-6 mb-4">
-                  Rudrapur
-                </h2>
-                <p className="text-lg font-serif leading-relaxed text-gray-600 mb-4">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus sunt quas eveniet, ratione sit eaque distinctio
-                  consequuntur, quam iusto ullam mollitia sint est laboriosam.
-                  Unde aliquam cumque nulla incidunt animi ipsum ad. Enim
-                  laudantium dolore laboriosam...
-                </p>
-              </React.Fragment>
-            ))}
+          <p className="font-bold text-2xl">Region</p>
+          <p className="text-xl">Uttarakhand | Dehradun</p>
         </div>
-        <div className="relative w-auto">
-          <div className="sticky top-1">
-            <div className="w-full md:w-auto flex justify-center items-start">
-              <img
-                className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-lg shadow-md"
-                src=""
-                alt="Rudrapur"
-              />
-            </div>
-          </div>
+        <div className="mt-3 mb-3 flex flex-col justify-center items-center">
+          <p className=" text-red-500 opacity-85 text-4xl">
+            <FontAwesomeIcon icon={faClock} />
+          </p>
+          <p className="font-bold text-2xl">Duration</p>
+          <p className="text-xl">{duration} days</p>
+        </div>
+        <div className="mt-3 mb-3 flex flex-col justify-center items-center">
+          <p className=" text-red-500 opacity-85 text-4xl">
+            <FontAwesomeIcon icon={faPersonHiking} />
+          </p>
+          <p className="font-bold text-2xl">Altitute</p>
+          <p className="text-xl">{altitude} Ft.</p>
+        </div>
+        <div className="mt-3 mb-3 flex flex-col justify-center items-center">
+          <p className=" text-red-500 opacity-85 text-4xl">
+            <FontAwesomeIcon icon={faMapLocationDot} />
+          </p>
+          <p className="font-bold text-2xl">Traking Area</p>
+          <p className="text-xl">{trakingarea} Km</p>
+        </div>
+        <div className="mt-3 mb-3 flex flex-col justify-center items-center">
+          <p className=" text-red-500 opacity-85 text-4xl">
+            <FontAwesomeIcon icon={faSignal} />
+          </p>
+          <p className="font-bold text-2xl">Grade</p>
+          <p className="text-xl">Easy to Moderate</p>
         </div>
       </div>
     </>
